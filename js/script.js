ef76ef7af9807cd11fd0e734d63269407b65c61d
VladGTest <vladislavfordev@gmail.com>
2023-10-15 16:12:18 +0200

    Add navigation menu for phone verst

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.hidden-header');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+
+})
+
+
+// closeElem.addEventListener('click', () => {
+//     menu.classList.remove('-active');
+// })
+
