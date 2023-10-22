0f229211f178f26736823446b6116e10b7f7feb9
VladGTest <vladislavfordev@gmail.com>
2023-10-22 17:31:37 +0200

    Changed from date 22.10.2023

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.hidden-header');
 
+const closeElem = document.querySelector('.hidden-header__close');
+
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 
-})
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
 
 
 // closeElem.addEventListener('click', () => {
